c5546b99012437ddd07e420ba84eaa9a43529887
LAPC\PHAM TAN TAI <taipham039@gmail.com>
2025-10-15 15:41:16 +0700

    INIT LAB3_UPADTE1

--- Lab3/Core/Inc/main.h
+++ Lab3/Core/Inc/main.h
@@ -58,6 +58,48 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BTN1_Pin GPIO_PIN_1
+#define BTN1_GPIO_Port GPIOA
+#define BTN2_Pin GPIO_PIN_2
+#define BTN2_GPIO_Port GPIOA
+#define BTN3_Pin GPIO_PIN_3
+#define BTN3_GPIO_Port GPIOA
+#define LED_Pin GPIO_PIN_5
+#define LED_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
+#define SEG0_Pin GPIO_PIN_0
+#define SEG0_GPIO_Port GPIOB
+#define SEG1_Pin GPIO_PIN_1
+#define SEG1_GPIO_Port GPIOB
+#define SEG2_Pin GPIO_PIN_2
+#define SEG2_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
+#define RED1_Pin GPIO_PIN_10
+#define RED1_GPIO_Port GPIOA
+#define RED2_Pin GPIO_PIN_11
+#define RED2_GPIO_Port GPIOA
+#define YEL1_Pin GPIO_PIN_12
+#define YEL1_GPIO_Port GPIOA
+#define YEL2_Pin GPIO_PIN_13
+#define YEL2_GPIO_Port GPIOA
+#define GRN1_Pin GPIO_PIN_14
+#define GRN1_GPIO_Port GPIOA
+#define GRN2_Pin GPIO_PIN_15
+#define GRN2_GPIO_Port GPIOA
+#define SEG3_Pin GPIO_PIN_3
+#define SEG3_GPIO_Port GPIOB
+#define SEG4_Pin GPIO_PIN_4
+#define SEG4_GPIO_Port GPIOB
+#define SEG5_Pin GPIO_PIN_5
+#define SEG5_GPIO_Port GPIOB
+#define SEG6_Pin GPIO_PIN_6
+#define SEG6_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
