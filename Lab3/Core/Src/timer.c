a1b062e8ab08b10c09757f3267f996bd7ae326fa
LAPC\PHAM TAN TAI <taipham039@gmail.com>
2025-11-21 21:30:08 +0700

    no message

--- Lab3/Core/Src/timer.c
+++ Lab3/Core/Src/timer.c
@@ -18,8 +18,17 @@
  int timer1_flag = 0;
  int timer1_inni = 0;
 
+ int timer2_counter = 0;
+  int timer2_flag = 0;
+  int timer2_inni = 0;
 
+  int traffic_timer_counter = 0;
+   int traffic_timer_flag = 0;
+   int traffic_timer_inni = 0;
 
+   int modify_timer_counter = 0;
+    int modify_timer_flag = 0;
+    int modify_timer_inni = 0;
  void set_Timer0(int duration0){
 	 timer0_inni = duration0 /TIMER_CYCLE;
 	 timer0_counter = timer0_inni;
@@ -30,7 +39,21 @@
 	 timer1_counter = timer1_inni;
  	 timer1_flag = 0;
  	 }
-
+ void set_Timer2(int duration0){
+ 	 timer2_inni = duration0 /TIMER_CYCLE;
+ 	 timer2_counter = timer2_inni;
+ 	 timer2_flag = 0;
+ 	 }
+  void set_Traffic_Timer(int duration1){
+	  traffic_timer_inni = duration1 /TIMER_CYCLE;
+	  traffic_timer_counter = traffic_timer_inni;
+	  traffic_timer_flag = 0;
+  	 }
+  void set_Modify_Timer(int duration1){
+	  modify_timer_inni = duration1 /TIMER_CYCLE;
+	  modify_timer_counter = modify_timer_inni;
+	  modify_timer_flag = 0;
+    	 }
 
  void timer_run(){
 	 if(timer0_counter > 0){
@@ -47,6 +70,27 @@
 			 timer1_counter = timer1_inni;
 		 }
 	 }
+	 if(timer2_counter > 0){
+	 		 timer2_counter--;
+	 		 if(timer2_counter == 0) {
+	 			 timer2_flag = 1;
+	 			 timer2_counter = timer1_inni;
+	 		 }
+	 	 }
+	 if(traffic_timer_counter > 0){
+		 traffic_timer_counter--;
+	 		 if(traffic_timer_counter == 0) {
+	 			traffic_timer_flag = 1;
+	 			traffic_timer_counter = traffic_timer_inni;
+	 		 }
+	 	 }
+	 if(modify_timer_counter > 0){
+		 modify_timer_counter--;
+	 		 if(modify_timer_counter == 0) {
+	 			modify_timer_flag = 1;
+	 			modify_timer_counter = modify_timer_inni;
+	 		 }
+	 	 }
  }
 
 
